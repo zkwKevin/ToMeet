8b566016a31fe6f0b4e33cb259d148cda549aa1a
kevin <zhukewei@zhukeweideMacBook-Pro.local>
2018-07-06 03:34:16 +1000

    Login Form

--- wwwroot/js/navbar.js
+++ wwwroot/js/navbar.js
@@ -2,7 +2,27 @@ $(".handel").on('click', function(){
     $("ul").toggleClass("stretch");
 });
 
+// $(document).click(function(e){
+//     if (!$(e.target).is(".handel")) {
+//          $("ul").removeClass("stretch");          
+//   }
+// });
+//open signup
 $(".signup").on('click', function(){
     $('#modal-wrapper').css({display: "block"});
+    $("ul").removeClass("stretch");
 });
+//close signup
+$(".close").on('click', function(){
+    $('#modal-wrapper').css({display: "none"});
+});
+//If user clicks anywhere outside of the modal, Modal will close
+$(document).click(function(e){
+    if (!$(e.target).is(".signup")) {
+        if (!$(e.target).parents().andSelf().is('.modal-content')){
+            $('#modal-wrapper').css({display: "none"});
+        }       
+  }
+});
+
 
